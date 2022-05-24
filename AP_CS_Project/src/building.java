29998442427124c92c2e2204d09dc5a10af45033
Victorier0101 <105460807+Victorier0101@users.noreply.github.com>
2022-05-24 14:53:39 -0700

    Add files via upload

---
+++ AP_CS_Project/src/building.java
@@ -0,0 +1,10 @@
+
+public class building {
+    private int cost;
+    
+    public building(){
+        
+    
+    
+    }
+}
---
+++ AP_CS_Project/src/spot.java
@@ -0,0 +1,6 @@
+
+public class spot {
+    private int soldiers=0;
+    private building type;
+    
+}
