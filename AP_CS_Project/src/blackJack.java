0782973ca60b4c92330f9b86c66461ba103e5145
Lo3wer <105460884+Lo3wer@users.noreply.github.com>
2022-06-03 15:05:09 -0700

    Add files via upload

--- AP_CS_Project/src/blackJack.java
+++ AP_CS_Project/src/blackJack.java
@@ -10,13 +10,38 @@
  */
 import java.util.ArrayList; // import the ArrayList class
 public class blackJack extends javax.swing.JFrame {
-
-    
+    ArrayList<Integer> cards = newCards();
+    ArrayList<Integer> hand = new ArrayList<>();
+    ArrayList<Integer> dealer = new ArrayList<>();
+    Boolean gamestate = false;
     
     public blackJack() {
+        this.dealer = new ArrayList<Integer>();
         initComponents();
     }
-
+    public ArrayList<Integer> newCards(){
+        ArrayList<Integer> cards = new ArrayList<Integer>();
+        for(int k = 1; k<=13; k++){
+            int temp = 4;
+            while(temp>0){
+                cards.add(k);
+                temp--;
+            }
+        }
+        return cards;
+    }
+    public int getTotal(ArrayList<Integer> c){
+        int total = 0;
+        for(int k = 0; k<c.size(); k++){
+            total+=c.get(k);
+        }
+        return total;
+    }
+    public void startingGame(){
+        dealer.add(cards.remove((int)(Math.random() * cards.size())));
+        hand.add(cards.remove((int)(Math.random() * cards.size())));
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -28,6 +53,9 @@ public class blackJack extends javax.swing.JFrame {
 
         hitButton = new javax.swing.JButton();
         standButton = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        buttonExit = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -45,25 +73,46 @@ public class blackJack extends javax.swing.JFrame {
             }
         });
 
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        jLabel1.setText("BlackJack");
+
+        jTextField1.setText("jTextField1");
+
+        buttonExit.setText("jButton1");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(121, 121, 121)
-                .addComponent(hitButton)
-                .addGap(18, 18, 18)
-                .addComponent(standButton)
-                .addContainerGap(107, Short.MAX_VALUE))
+                .addGap(19, 19, 19)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 214, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(hitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(standButton, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 159, Short.MAX_VALUE)
+                        .addComponent(buttonExit)
+                        .addGap(32, 32, 32))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(50, 50, 50)
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(67, 67, 67)
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 80, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(standButton)
                     .addComponent(hitButton)
-                    .addComponent(standButton))
-                .addContainerGap(221, Short.MAX_VALUE))
+                    .addComponent(buttonExit))
+                .addContainerGap())
         );
 
         pack();
@@ -110,19 +159,13 @@ public class blackJack extends javax.swing.JFrame {
                 new blackJack().setVisible(true);
             }
         });
-        ArrayList<Integer> cards = new ArrayList<Integer>();
-        for(int k = 1; k<=13; k++){
-            int temp = 4;
-            while(temp>0){
-                cards.add(k);
-                temp--;
-            }
-        }
-        
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton buttonExit;
     private javax.swing.JButton hitButton;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JTextField jTextField1;
     private javax.swing.JButton standButton;
     // End of variables declaration//GEN-END:variables
 }
